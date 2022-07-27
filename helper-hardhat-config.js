341df3a71f8f67da2095bc5e18006afa29063898
AN ❦ <108299225+krakxn@users.noreply.github.com>
2022-07-27 18:31:37 +0530

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,14 +6,14 @@ const networkConfig = {
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionId: "0",
+        subscriptionId: "0", // insert a subscription ID by making one here please -- else it will not fix ur problem
         callbackGasLimit: "500000", //500,000
         interval: "30",
     },
     31337: {
         name: "hardhat",
         entranceFee: ethers.utils.parseEther("0.01"),
-        gasdLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         callbackGasLimit: "500000", //500,000
         interval: "30",
     },
